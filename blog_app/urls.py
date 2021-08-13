ebfb04a1dccb560833dd8e25058db9447c571e2c
zendus <uzendujmike@gmail.com>
2021-08-13 22:14:50 +0100

    added routes and html pages of sections except login

--- blog_app/urls.py
+++ blog_app/urls.py
@@ -4,4 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.all_posts, name='all_posts'),
     path('posts/<int:pk>/', views.read_post, name='read_post'),
+    path('articles/', views.all_articles, name='all_articles'),
+    path('poems/', views.all_poems, name='all_poems'),
+    path('quotesandvibes/', views.all_yarns, name='all_yarns'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login, name='login'),
 ]
--- blog_app/views.py
+++ blog_app/views.py
@@ -3,7 +3,7 @@ from blog_app.models import Post, Comment
 
 # Create your views here.
 def all_posts(request):
-    posts = Post.objects.all()
+    posts = Post.objects.all().order_by('pk').reverse()[1:]
     lastest_post = Post.objects.latest('created_at')
     context = {
         'posts': posts,
@@ -17,3 +17,26 @@ def read_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     return render(request, 'blog_app/read_post.html', {'post': post})
+
+def all_articles(request):
+    articles = Post.objects.filter(section='article')
+
+    return render(request, 'blog_app/all_articles.html', {'articles': articles})
+
+def all_poems(request):
+    poems = Post.objects.filter(section='poem')
+
+    return render(request, 'blog_app/all_poems.html', {'poems': poems})
+    
+
+def all_yarns(request):
+    yarns = Post.objects.filter(section='yarn')
+
+    return render(request, 'blog_app/all_yarns.html', {'yarns': yarns})
+
+def contact(request):
+    
+    return render(request, 'blog_app/contact.html')
+
+def login(request):
+    pass
