64917c0480054cb366824e0d11dd7c1998aff05a
zendus <uzendujmike@gmail.com>
2021-08-22 21:59:13 +0100

    edited debug value

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = (os.environ.get('DEBUG_VALUE') == 'True')
 
 ALLOWED_HOSTS = ['zendusblog.herokuapp.com']
 
